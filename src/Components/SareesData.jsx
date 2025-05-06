5f3cbe1bcc71e658bc3b112344bfcbda08af017a
MaheshDurishetti <maheshdurishetti7370@gmail.com>
2025-05-06 18:24:17 +0530

    asserts moved to public folder

--- src/Components/SareesData.jsx
+++ src/Components/SareesData.jsx
@@ -1,14 +1,15 @@
 export const sarees=[
-  { id: 1, name: 'Red Silk Saree', img: "src/assets/Images/saare1.jpg", count: 0 },
-  { id: 2, name: 'Blue Cotton Saree', img: 'src/assets/Images/saare2.jpg', count: 0 },
-  { id: 3, name: 'Green Designer Saree', img: 'src/assets/Images/saare3.jpg', count: 0 },
-  { id: 4, name: 'Red Silk Saree', img: "src/assets/Images/saare1.jpg", count: 0 },
-  { id: 5, name: 'Blue Cotton Saree', img: 'src/assets/Images/saare2.jpg', count: 0 },
-  { id: 6, name: 'Green Designer Saree', img: 'src/assets/Images/saare3.jpg', count: 0 },
-  { id: 7, name: 'Red Silk Saree', img: "src/assets/Images/saare1.jpg", count: 0 },
-  { id: 8, name: 'Blue Cotton Saree', img: 'src/assets/Images/saare2.jpg', count: 0 },
-  { id: 9, name: 'Green Designer Saree', img: 'src/assets/Images/saare3.jpg', count: 0 },{ id: 1, name: 'Red Silk Saree', img: "src/assets/Images/saare1.jpg", count: 0 },
-  { id: 10, name: 'Blue Cotton Saree', img: 'src/assets/Images/saare2.jpg', count: 0 },
-  { id: 11, name: 'Green Designer Saree', img: 'src/assets/Images/saare3.jpg', count: 0 }
+  { id: 1, name: 'Red Silk Saree', img: "/Images/saare1.jpg", count: 0 },
+  { id: 2, name: 'Blue Cotton Saree', img: '/Images/saare2.jpg', count: 0 },
+  { id: 3, name: 'Green Designer Saree', img: '/Images/saare3.jpg', count: 0 },
+  { id: 4, name: 'Red Silk Saree', img: "/Images/saare1.jpg", count: 0 },
+  { id: 5, name: 'Blue Cotton Saree', img: '/Images/saare2.jpg', count: 0 },
+  { id: 6, name: 'Green Designer Saree', img: '/Images/saare3.jpg', count: 0 },
+  { id: 7, name: 'Red Silk Saree', img: "/Images/saare1.jpg", count: 0 },
+  { id: 8, name: 'Blue Cotton Saree', img: '/Images/saare2.jpg', count: 0 },
+  { id: 9, name: 'Green Designer Saree', img: '/Images/saare3.jpg', count: 0 },
+  { id: 1, name: 'Red Silk Saree', img: "/Images/saare1.jpg", count: 0 },
+  { id: 10, name: 'Blue Cotton Saree', img: '/Images/saare2.jpg', count: 0 },
+  { id: 11, name: 'Green Designer Saree', img: '/Images/saare3.jpg', count: 0 }
 ]
   
